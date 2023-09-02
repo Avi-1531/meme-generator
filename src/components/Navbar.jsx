aff84170ea1aa8d6cfd812522a36213b04d9a297
Avi Chauhan <avichauhan1531@gmail.com>
2023-09-02 22:24:52 +0530

    changed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ const Navbar = () => {
   return (
     <div className="bg-gradient-to-br from-purple-400 to-purple-500 h-[65px] flex items-center justify-between">
       <div className="flex  items-center">
-        <img src="/meme.svg" className="w-[32px] h-[36px] m-1" alt="" />
+        <img src="./meme.svg" className="w-[32px] h-[36px] m-1" alt="" />
         <span className="text-white text-xl p-1 text-[20.748px]">
           Meme Generator
         </span>
